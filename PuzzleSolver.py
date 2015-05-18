8c4eae9446d5355a55102d27d2e44bb32a449eec
Malcolm White <malcolmdavidwhite@gmail.com>
2015-05-17 23:52:52 -0500

    More optimization
    Removed unnecessary square construction and fixed previous pruning

--- PuzzleSolver.py
+++ PuzzleSolver.py
@@ -17,54 +17,54 @@ class PuzzleSolver(object):
         self.last_col_index = self.grid.shape[1] - 1
         self.original_grid = copy(self.grid)
         self.grid_vis = zeros(shape=self.grid.shape)
-        self.recent_square = None
-        self.current_max_size = 0
-        self.previous_max_size = None
         self.solution = Pz.PuzzleSolution(self.puzzle_id)
 
     def top_left_solve(self):
         squares_remain = True
+        previous_max_size = None
 
         while squares_remain:
+            found_block = False
             squares_remain = False
             valid_size = 0
             test_size = 1
+            start_col = 0
+            start_row = 0
+            current_max_size = 0
+
             for row in range(0, self.last_row_index + 1):
                 for col in range(0, self.last_col_index + 1):
                     if self.grid[row, col]:
                         squares_remain = True
-                        test_square = Sq.Square(col=col, row=row, size=test_size, row_major=self.ROW_MAJOR)
-                        while self.is_valid_square(test_square):
+                        while self.is_valid_square(start_col = col, start_row = row, size = test_size):
                             test_size += 1
-                            test_square = Sq.Square(col=col, row=row, size=test_size, row_major=self.ROW_MAJOR)
-
                         valid_size = test_size - 1
-
-                        if valid_size > self.current_max_size:
-                            self.current_max_size = valid_size
-                            self.recent_square = Sq.Square(col=col, row=row, size=valid_size, row_major=self.ROW_MAJOR)
-                            if self.previous_max_size is not None and valid_size > self.previous_max_size:
+                        if valid_size > current_max_size:
+                            current_max_size = valid_size
+                            start_col = col
+                            start_row = row
+                            found_block = True
+                            if previous_max_size is not None and valid_size == previous_max_size:
                                 break
                     if col + valid_size > self.last_col_index:
                         break
-                if self.previous_max_size is not None and valid_size > self.previous_max_size:
+                if previous_max_size is not None and valid_size == previous_max_size:
                             break
                 if row + valid_size > self.last_row_index:
                     break
 
             # After a full grid sweep, check to see if any squares were found
             # If none were found, the partition has completed
-            if self.recent_square is not None:
-                self.save_square(self.recent_square)
+            if found_block:
+                self.save_square(start_row=start_row, start_col=start_col, size=current_max_size)
+                previous_max_size = current_max_size
 
         if self.DEBUG_VIZ:
             self.visualize_grids(self.original_grid, self.grid_vis)
 
-    def is_valid_square(self, square):
-        start_row = square.get_row_start(self.ROW_MAJOR)
-        end_row = square.get_row_end(self.ROW_MAJOR)
-        start_col = square.get_col_start(self.ROW_MAJOR)
-        end_col = square.get_col_end(self.ROW_MAJOR)
+    def is_valid_square(self, start_col, start_row, size):
+        end_col = start_col + size - 1
+        end_row = start_row + size - 1
 
         if end_row > self.last_row_index or end_col > self.last_col_index:
             return False
@@ -75,16 +75,11 @@ class PuzzleSolver(object):
 
         return True
 
-    def save_square(self, square):
-        start_col = square.get_col_start(self.ROW_MAJOR)
-        start_row = square.get_row_start(self.ROW_MAJOR)
-        end_col = square.get_col_end(self.ROW_MAJOR)
-        end_row = square.get_row_end(self.ROW_MAJOR)
+    def save_square(self, start_col, start_row, size):
+        end_col = start_col + size - 1
+        end_row = start_row + size - 1
         square_num = len(self.solution.get_squares())
-        self.solution.add_square(square)
-        self.recent_square = None
-        self.previous_max_size = self.current_max_size
-        self.current_max_size = 0
+        self.solution.add_square(Sq.Square(row=start_row, col=start_col, row_major=self.ROW_MAJOR, size=size))
         for col in xrange(start_col, end_col + 1):
             for row in xrange(start_row, end_row + 1):
                 self.grid[row, col] = False
