bacf6744cba739603936470acd1dad37af231e48
Malcolm White <malcolmdavidwhite@gmail.com>
2015-05-18 00:13:45 -0500

    Cleaned up vizual debug methods

--- PuzzleSolver.py
+++ PuzzleSolver.py
@@ -1,12 +1,11 @@
-from numpy import asarray, copy, zeros
-import matplotlib.pyplot as plt
+from numpy import asarray
 
 import Square as Sq
 import PuzzleSolution as Pz
 
 
 class PuzzleSolver(object):
-    DEBUG_VIZ = False
+    DEBUG_VIZ = True
 
     def __init__(self, puzzle):
         self.puzzle = puzzle
@@ -14,8 +13,6 @@ class PuzzleSolver(object):
         self.grid = self.get_grid_from_puzzle(puzzle=puzzle)
         self.grid_rows = self.grid.shape[0]
         self.grid_columns = self.grid.shape[1]
-        self.original_grid = copy(self.grid)
-        self.grid_vis = zeros(shape=self.grid.shape)
         self.solution = Pz.PuzzleSolution(self.puzzle_id)
 
     def top_left_solve(self):
@@ -49,7 +46,7 @@ class PuzzleSolver(object):
                         break
                 if previous_max_size is not None and valid_size == previous_max_size:
                             break
-                if row + valid_size > self.grid_rows:
+                elif row + valid_size > self.grid_rows:
                     break
 
             # After a full grid sweep, check to see if any squares were found
@@ -58,8 +55,6 @@ class PuzzleSolver(object):
                 self.save_square(start_row=start_row, start_col=start_col, size=current_max_size)
                 previous_max_size = current_max_size
 
-        if self.DEBUG_VIZ:
-            self.visualize_grids(self.original_grid, self.grid_vis)
 
     def is_valid_square(self, start_col, start_row, size):
         end_col = start_col + size
@@ -75,35 +70,17 @@ class PuzzleSolver(object):
         return True
 
     def save_square(self, start_col, start_row, size):
-        end_col = start_col + size - 1
-        end_row = start_row + size - 1
-        square_num = len(self.solution.get_squares())
+        end_col = start_col + size
+        end_row = start_row + size
         self.solution.add_square(Sq.Square(row=start_row, col=start_col, size=size))
-        for col in xrange(start_col, end_col + 1):
-            for row in xrange(start_row, end_row + 1):
+        for row in xrange(start_row, end_row):
+            for col in xrange(start_col, end_col):
                 self.grid[row, col] = False
-                if self.DEBUG_VIZ:
-                    self.grid_vis[row, col] = square_num
-
-    def get_solution(self):
-        return self.solution
-
-    @staticmethod
-    def visualize_grids(grid, split_grid):
-        base_plot = plt.imshow(grid)
-        base_plot.set_interpolation('nearest')
-
-        plt.colorbar()
-        plt.show()
-
-        end_plot = plt.imshow(split_grid)
-        end_plot.set_interpolation('nearest')
-
-        plt.colorbar()
-        plt.show()
 
     @staticmethod
     def get_grid_from_puzzle(puzzle):
         puzzle_grid = puzzle["puzzle"]
-        array = asarray(puzzle_grid, order='F')
-        return array
+        return asarray(puzzle_grid)
+
+    def get_solution(self):
+        return self.solution
